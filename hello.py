d5a11475bed37310379e721881d48179cdc47975
Krytoi <radikchickenn@gmail.com>
2019-11-08 00:56:06 +0300

    первая версия

---
+++ hello.py
@@ -0,0 +1,14 @@
+# c = 'введите число'
+# a = ', 1 или 2'
+# k = 'keys'
+# b = f'{c} {a}{k}{k}'
+# print(b)
+
+#    a = 'Darove'.upper()
+ #   b = 'LASLD'.lower()
+#    c = '{} {}'.format(a, b)
+ #   print(c)
+
+a = '  Кирилл'
+print(a)
+print(a.strip())
